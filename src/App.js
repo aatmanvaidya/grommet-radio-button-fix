a3f9eaf18ad24d306d87491f124f298c25b5f846
Aatman Vaidya <aatmanvaidya@gmail.com>
2023-10-09 17:32:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useState, useContext } from "react";
+import {
+  Box,
+  grommet,
+  Grommet,
+  Text,
+  Form,
+  FormField,
+  RadioButtonGroup,
+  RadioButton
+} from "grommet";
+import { deepMerge } from "grommet/utils";
+
+const theme = deepMerge(grommet, {
+  global: {
+    colors: {
+      brand: "#228BE6",
+    },
+    font: {
+      family: "Roboto",
+      size: "14px",
+      height: "20px",
+    },
+  },
+});
+
+const App = () => {
+  const [selectedOption, setSelectedOption] = useState(true);
+
+  const handleSubmit = () => {
+    console.log('Selected option:', selectedOption);
+  };
+
+  return (
+    <Grommet theme={theme} full>
+      {/* <Box align="center" pad="large">
+        <Text>testing RadioButtonGroup</Text>
+        <Form onSubmit={handleSubmit}>
+          <FormField name="question" label="Question">
+            <RadioButtonGroup
+              name="question"
+              options={[
+                { label: 'Yes', value: true },
+                { label: 'No', value: false },
+              ]}
+              value={selectedOption}
+              onChange={(e) => setSelectedOption(e.target.value)}
+            />
+          </FormField>
+          <button type="submit">Submit</button>
+        </Form>
+      </Box> */}
+      <Box align="center" pad="large">
+        <Form onSubmit={handleSubmit}>
+          <FormField name="question" label="Question">
+            <RadioButton
+              name="question"
+              label="Yes"
+              value={true}
+              checked={selectedOption === true}
+              onChange={() => setSelectedOption(true)}
+            />
+            <RadioButton
+              name="question"
+              label="No"
+              value={false}
+              checked={selectedOption === false}
+              onChange={() => setSelectedOption(false)}
+            />
+          </FormField>
+          <button type="submit">Submit</button>
+        </Form>
+      </Box>
+    </Grommet>
+  );
+};
+
+export default App;
